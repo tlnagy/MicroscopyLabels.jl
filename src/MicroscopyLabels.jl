bf713a7ad4a69c4687c336c9a1dee03f93461930
Tamas Nagy <tamas@tamasnagy.com>
2019-09-19 14:25:39 -0700

    add unitful example to docstring

--- src/MicroscopyLabels.jl
+++ src/MicroscopyLabels.jl
@@ -26,14 +26,31 @@ with the units `units`. Images are required to have at least the `x`, `y`, and
 xyt axes. If additional axes are present, the timestamp will be written to all
 of them.
 
-### Example
+### Simple example
 
-```jldoctest; output = false
+```jldoctest ex; output = false
 using AxisArrays
 using MicroscopyLabels
 
 tmp = AxisArray(zeros(200, 200, 5), Axis{:x}(1:200), Axis{:y}(1:200), Axis{:time}(1:5))
 timestamp!(tmp)
+
+# output
+
+
+```
+
+### Unitful example
+
+We can also add units to the time axis and have these be embedded in the image
+
+```jldoctest ex; output=false
+using Unitful: s, minute
+
+tmp = AxisArray(zeros(200, 200, 5), Axis{:x}(1:200), Axis{:y}(1:200),
+                Axis{:time}(0s:45.0s:180s))
+# convert the units to minutes
+timestamp!(tmp, units=minute)
 
 # output
 
